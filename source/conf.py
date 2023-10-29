742627fa91eb1c00a1250f4ff7b51f5f81229f32
Bijan Sayyafzadeh <82403633+BijanSeif@users.noreply.github.com>
2023-10-29 17:58:55 +0330

    First Release
    The first configuration of BraineryWiz's Documentation using a very new and powerful sphinx-theme

---
+++ source/conf.py
@@ -0,0 +1,37 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+project = 'BraineryWizDocumentation'
+copyright = '2023, Bijan Sayyafzaden - Silvia Mazzoni'
+author = 'Bijan Sayyafzaden - Silvia Mazzoni'
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+extensions = []
+
+templates_path = ['_templates']
+exclude_patterns = []
+
+
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+html_theme = "pydata_sphinx_theme"
+
+html_static_path = ['_static']
+
+html_theme_options={
+    'navigation_with_keys':False
+    }
+    
+
+html_context = {"default_mode": "light"}
+html_short_title = "BraineryWiz"
+html_logo = "_static/BraineryWizLogo.png"
